0ab896236263dececce55959c143bc354a7f37fb
Robins <louis@robins-village.com>
2022-02-24 15:07:17 +0100

    add python files

---
+++ Crowd-counting/__init__.py
@@ -0,0 +1,5 @@
+import dataset
+import image
+import model
+import train
+import utils
---
+++ Crowd-counting/utils.py
@@ -0,0 +1,18 @@
+import h5py
+import torch
+import shutil
+
+def save_net(fname, net):
+    with h5py.File(fname, 'w') as h5f:
+        for k, v in net.state_dict().items():
+            h5f.create_dataset(k, data=v.cpu().numpy())
+def load_net(fname, net):
+    with h5py.File(fname, 'r') as h5f:
+        for k, v in net.state_dict().items():        
+            param = torch.from_numpy(np.asarray(h5f[k]))         
+            v.copy_(param)
+            
+def save_checkpoint(state, is_best,task_id, filename='/content/gdrive/My Drive/TFE crowd counting/CSRNet-pytorch/checkpoint.pth.tar'):
+    torch.save(state, filename)
+    if is_best:
+        shutil.copyfile(filename,'/content/gdrive/My Drive/TFE crowd counting/CSRNet-pytorch/model_best.pth.tar')            
