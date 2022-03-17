469811971f1286b4e62923eb643760fceed7c4fe
Robins <louis@robins-village.com>
2022-03-17 14:42:36 +0100

    gitignore

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Crowd-counting',
+    name='Crowd_counting',
     version='0.2',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='A package for crowd-counting in Uclouvain auditorium',
     long_description=open('README.md').read(),
-    install_requires=['numpy','tensorflow','cv2','os','scipy','random','h5py','PIL','json','warnings'],
+    install_requires=['numpy','tensorflow','scipy','random','h5py','PIL','json','warnings'],
     url='https://github.com/lrobins1/crowd_counting',
     author='Henri Collin & Louis Robins',
     author_email='henri.collin@student.uclouvain.be & louis.robins@tudent.uclouvain.be'
