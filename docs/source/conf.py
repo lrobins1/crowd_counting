1e01deb3308775d4c298b6ee44ada1d9f3c187d6
Robins <louis@robins-village.com>
2022-05-19 15:40:32 +0200

    try conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,6 +39,7 @@ release = '0.6'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'readthedocs_ext.readthedocs'
     'sphinx.ext.autodoc',
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
