cee47ec41396435285a3352c307c675f4a9a69c5
Robins <louis@robins-village.com>
2022-05-20 09:33:43 +0200

    ss

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,9 +15,6 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
-sys.path.insert(0, os.path.abspath('../'))
-sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('docs/source'))
 # -- Project information -----------------------------------------------------
 
 project = 'Crowd_counting'
